5a3652197f9d8ed751235131c19b489840c271d9
Daria <ghenegrise@gmail.com>
2022-10-17 23:21:10 +0500

    all

--- homework5/task4.py
+++ homework5/task4.py
@@ -1,3 +1,39 @@
-# 4. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных. Входные и выходные данные хранятся в отдельных текстовых файлах.
+def archive(text):
+    count = 1
+    res = ''
+    for i in range(len(text)-1):
+        if text[i] == text[i+1]:
+            count += 1
+        else:
+            res = res + str(count) + text[i]
+            count = 1
+    if count > 1 or (text[len(text)-2] != text[-1]):
+        res = res + str(count) + text[-1]
+    return res
 
-# Пример: aaaaaaabbbbbbcccccccccd => 7a6b9c1d или 11a3b7c1d => aaaaaaaaaaabbbcccccccd
+def unpacking(text):
+    num = ''
+    res = ''
+    for i in range(len(text)):
+        if not text[i].isalpha():
+            num += text[i]
+        else:
+            res = res + text[i] * int(num)
+            num = ''
+    return res
+
+def write_file(name, st):
+    with open(name, 'w') as data:
+        data.write(st)
+
+welcome = int(input('Будем сжимать (0) или восстанавливать (1) данные? Введи число: '))
+
+x = input('Введите текст: ')
+write_file("task5_text.txt", x)
+
+if welcome == 0:
+    print('Текст после кодировки: ', archive(x))
+    write_file("task5_archive.txt", archive(x))
+else:
+    print('Текст после дешифровки: ', unpacking(x))
+    write_file("task5_unpacking.txt", unpacking(x))
