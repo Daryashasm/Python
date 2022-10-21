f428b6ad20f26eb9c329ba057168fef138c54a25
Daria <ghenegrise@gmail.com>
2022-10-21 16:59:28 +0500

    all

--- homework6/task2.py
+++ homework6/task2.py
@@ -2,11 +2,11 @@ import random
 L = int(input('Введите длину массива, L = '))
 array = [random.randint(1, 100) for i in range (L)]
 print('Массив:', array)
-even_numbers = [y for x, y in enumerate(array) if x % 2 != 0]
-print('Массив:', even_numbers)
+even = [y for x, y in enumerate(array) if x % 2 != 0]
+print('Массив:', even)
 def sum(x):
     sum = 0
     for i in range (len(x)):
         sum = sum + x[i]
     return sum
-print('Сумма элементов, находящихся на нечетных позициях', sum(even_numbers))
+print('Сумма элементов, находящихся на нечетных позициях', sum(even))
